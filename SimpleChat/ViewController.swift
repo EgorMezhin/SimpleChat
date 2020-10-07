ae321bd4cbcba70698a5bb6196ce1dea1794a0b4
EgorMezhin <49487690+EgorMezhin@users.noreply.github.com>
2020-10-07 23:49:59 +0300

    Initial commit

---
+++ SimpleChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleChat
+//
+//  Created by Egor Lass on 07.10.2020.
+//  Copyright © 2020 Egor Mezhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
